4b649cfcb05654dea41e3f0d6304fa17684733e6
AlondraCordoba <alondracordoba0720@gmail.com>
2022-02-02 11:04:51 -0600

    Styling added

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_01_212900) do
-
+ActiveRecord::Schema.define(version: 20_220_201_212_900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,5 +35,4 @@ ActiveRecord::Schema.define(version: 2022_02_01_212900) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
 end
